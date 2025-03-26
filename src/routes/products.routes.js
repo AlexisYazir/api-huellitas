8d01a3aab337ef0e52ddcf9a9c61e19657ee1ea2
AlexisYazir <20230060@uthh.edu.mx>
2025-03-26 00:55:13 -0600

    Update products.routes.js
    cambios

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -10,21 +10,21 @@ const router = Router();
 //para consultar todas las tareas
 router.get("/product", getProducts)
 
-router.get("/products", authRequired, getProducts)
+router.get("/products", getProducts)
 
 router.get("/view-details/:id", getProduct)
 
 //para traer prodcuto 1 se espera un :id OK
-router.get("/add-product/:id", authRequired, getProduct)
+router.get("/add-product/:id", getProduct)
 
 //para crear productos, verificar usuario aun 
 router.post("/producto",validateSchema(createProductSchema) , createProduct)
 
 //para eliminar producto se espera un :id
-router.delete("/products/:id", authRequired, deleteProduct)
+router.delete("/products/:id", deleteProduct)
 
 //para actualizar una tarea se espera un :id OKEYYYY
-router.put("/products/:id", authRequired, updateProduct)
+router.put("/products/:id", updateProduct)
 
 //para obteenr las marcas y categorias
 router.get('/marcas', getMarcas); // Ruta para obtener las marcas
